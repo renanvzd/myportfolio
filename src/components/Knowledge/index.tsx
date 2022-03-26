3e4f2ce97a48ac6f8530a053deef4418f1628fb4
Renan Veronez Drechsler <renanvz@outlook.com>
2022-03-26 16:35:50 +0000

    Add Typescript React-Icon

--- src/components/Knowledge/index.tsx
+++ src/components/Knowledge/index.tsx
@@ -1,5 +1,6 @@
 import { AiFillHtml5 } from 'react-icons/ai';
 import { FaCss3Alt, FaReact } from 'react-icons/fa';
+import { SiTypescript } from 'react-icons/si';
 import { IoLogoJavascript } from 'react-icons/io5';
 import SectionTitle from '../SectionTitle';
 import KnowledgeItem from './KnowledgeItem';
@@ -14,6 +15,7 @@ function Knowledges() {
         <KnowledgeItem title="CSS" icon={<FaCss3Alt />} />
         <KnowledgeItem title="Javascript" icon={<IoLogoJavascript />} />
         <KnowledgeItem title="React" icon={<FaReact />} />
+        <KnowledgeItem title="Typescript" icon={<SiTypescript />} />
       </section>
     </Container>
   );
