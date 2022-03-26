cd433210b4acc103e0fd7492bd6c974d6078928c
Renan Veronez Drechsler <renanvz@outlook.com>
2022-03-26 16:43:20 +0000

    Change Position Experiences

--- src/components/Experiences/index.tsx
+++ src/components/Experiences/index.tsx
@@ -9,14 +9,9 @@ function Experiencias() {
 
       <section>
         <ExperienceItem
-          year="2019 to nowadays"
-          title="Works and Projects"
-          description="You can check out the projects I've been working right here on this website"
-        />
-        <ExperienceItem
-          year="2018 to 2020"
-          title="Systems for Internet degree"
-          description="This degree allowed me to build and develop basic and fundamental skills."
+          year="2010 to 2014"
+          title="Bachelor Degree in Business Administration"
+          description="I believe developers also need to understand business. So, this degree helps me to think about the business problem."
         />
         <ExperienceItem
           year="2013 to 2021"
@@ -24,9 +19,14 @@ function Experiencias() {
           description="I worked as a product owner at a brazilian bank (Banrisul) that has 4.3 million customers."
         />
         <ExperienceItem
-          year="2010 to 2014"
-          title="Bachelor Degree in Business Administration"
-          description="I believe developers also need to understand business. So, this degree helps me to think about the business problem."
+          year="2018 to 2020"
+          title="Systems for Internet degree"
+          description="This degree allowed me to build and develop basic and fundamental skills."
+        />
+        <ExperienceItem
+          year="2020 to nowadays"
+          title="Works and Projects"
+          description="You can check out the projects I've been working right here on this website"
         />
       </section>
     </Container>
