22a76b2cefe5b2c52e74e50cfeda377b9cf7d380
Renan Veronez Drechsler <renanvz@outlook.com>
2022-06-29 16:02:13 +0100

    Description

--- src/components/Experiences/index.tsx
+++ src/components/Experiences/index.tsx
@@ -11,22 +11,22 @@ function Experiencias() {
         <ExperienceItem
           year="2010 to 2014"
           title="Business - Bachelor Degree"
-          description="I believe developers also need to understand business rules. So, this degree give me a background to have a macroperspective view of the business."
+          description="I believe a developer also needs to understand about business rules. I gained a valuable background during this degree."
         />
         <ExperienceItem
           year="2013 to 2021"
           title="Product Owner"
-          description="I worked as a product owner at a brazilian bank (Banrisul) that has 4.3 million customers."
+          description="I worked as a Product Owner at a Brazilian bank (Banrisul). There are 4.3 million customers and 514 agencies in this bank."
         />
         <ExperienceItem
           year="2018 to 2020"
           title="Internet Systems - Associate Degree"
-          description="This degree allowed me to build and develop solid and fundamental skills."
+          description="This degree allowed me to build and develop solid and fundamental skills. I can execute tasks with agility and great quality."
         />
         <ExperienceItem
           year="2020 to nowadays"
           title="Works and Projects"
-          description="You can check out the projects I've been working here on this website"
+          description="I have been using the languages and technologies most used by companies. Visit my portfolio and check my skills for yourself."
         />
       </section>
     </Container>
