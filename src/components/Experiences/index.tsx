2b560549525fbf171547736e6d17ed00d9adeb24
Renan Veronez Drechsler <renanvz@outlook.com>
2023-01-02 11:55:34 +0000

    Adjusts

--- src/components/Experiences/index.tsx
+++ src/components/Experiences/index.tsx
@@ -15,18 +15,18 @@ function Experiencias() {
         />
         <ExperienceItem
           year="2013 to 2021"
-          title="Product Owner"
-          description="I worked as a Product Owner at Banrisul (Brazilian Bank) which has 4.3 million customers and 514 agencies. There I have developed great abilities to multitask and adhere to a project's schedule."
+          title="Business Analyst"
+          description="I worked as a Business Analyst at Banrisul (Brazilian Bank) which has 4.3 million customers and 514 branches. I had the opportunity to acquire great abilities to multitask and adhere to a project's schedule."
         />
         <ExperienceItem
           year="2018 to 2020"
-          title="Associate Degree in Computer Programming (Level 7)"
-          description="This degree allowed me to build and develop solid and fundamental skills. I can execute tasks with agility and great quality."
+          title="Associate Degree in Computer Programming (Level 6)"
+          description="This degree, based on PHP and Javascript, has brought me to build and develop solid and fundamental skills in Computer Programming."
         />
         <ExperienceItem
-          year="2020 to nowadays"
-          title="Jobs and Projects"
-          description="I have been using the languages and technologies most used by companies. You can see my portfolio below and check my skills for yourself."
+          year="2021 to 2022"
+          title="Frontend Developer"
+          description="I worked as a Frontend Developer where I used to develop and implement highly responsive user interface components based on Javascript and ReactJS."
         />
       </section>
     </Container>
